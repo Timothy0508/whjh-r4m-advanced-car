af2531c9a5ab0d98027b48fde8e5a2c1c008c4ea
Timothy0508 <99249271+Timothy0508@users.noreply.github.com>
2023-07-17 10:58:26 +0800

    bugfix

--- main.ts
+++ main.ts
@@ -1,21 +1,7 @@
 function Left () {
     sensors.DDMmotor(
     AnalogPin.P13,
-    0,
-    AnalogPin.P14,
-    127
-    )
-    sensors.DDMmotor(
-    AnalogPin.P15,
-    0,
-    AnalogPin.P16,
-    127
-    )
-}
-function Foward () {
-    sensors.DDMmotor(
-    AnalogPin.P13,
-    0,
+    1,
     AnalogPin.P14,
     127
     )
@@ -25,17 +11,56 @@ function Foward () {
     AnalogPin.P16,
     127
     )
+}
+function Foward () {
+    sensors.DDMmotor(
+    AnalogPin.P13,
+    0,
+    AnalogPin.P14,
+    255
+    )
+    sensors.DDMmotor(
+    AnalogPin.P15,
+    1,
+    AnalogPin.P16,
+    255
+    )
+}
+function section2 () {
+    Right()
+    basic.pause(1500)
+    Foward()
+    basic.pause(200)
+    Backward()
+    basic.pause(800)
+    Foward()
+    basic.pause(500)
+    Left()
+    basic.pause(1500)
+}
+input.onPinPressed(TouchPin.P2, function () {
+    start = 1
+})
+function section4 () {
+    Right()
+    basic.pause(1500)
+    Backward()
+    basic.pause(500)
+    Foward()
+    basic.pause(500)
+    Left()
+    basic.pause(1500)
 }
 function Right () {
     sensors.DDMmotor(
     AnalogPin.P13,
-    1,
+    0,
     AnalogPin.P14,
     127
     )
     sensors.DDMmotor(
     AnalogPin.P15,
-    1,
+    0,
     AnalogPin.P16,
     127
     )
@@ -54,18 +79,50 @@ function Stop () {
     0
     )
 }
+function section5 () {
+    Right()
+    basic.pause(1500)
+    Foward()
+    basic.pause(200)
+    Backward()
+    basic.pause(800)
+    Stop()
+    basic.pause(99999999999999999999999)
+}
+function Backward () {
+    sensors.DDMmotor(
+    AnalogPin.P13,
+    1,
+    AnalogPin.P14,
+    255
+    )
+    sensors.DDMmotor(
+    AnalogPin.P15,
+    0,
+    AnalogPin.P16,
+    255
+    )
+}
+let start = 0
+start = 0
 let section = 0
+pins.setPull(DigitalPin.P2, PinPullMode.PullUp)
 basic.forever(function () {
-    if (pins.digitalReadPin(DigitalPin.P1) == 0 && pins.digitalReadPin(DigitalPin.P8) == 0) {
-        Foward()
-    } else if (pins.digitalReadPin(DigitalPin.P1) == 0 && pins.digitalReadPin(DigitalPin.P8) == 1) {
-        Left()
-    } else if (pins.digitalReadPin(DigitalPin.P1) == 1 && pins.digitalReadPin(DigitalPin.P8) == 0) {
-        Right()
-    } else if (pins.digitalReadPin(DigitalPin.P1) == 1 && pins.digitalReadPin(DigitalPin.P8) == 1) {
-        section += 1
-        basic.showNumber(section)
-        Foward()
-        basic.pause(500)
+    if (start == 1) {
+        if (pins.digitalReadPin(DigitalPin.P1) == 0 && pins.digitalReadPin(DigitalPin.P8) == 0) {
+            Foward()
+        } else if (pins.digitalReadPin(DigitalPin.P1) == 0 && pins.digitalReadPin(DigitalPin.P8) == 1) {
+            Right()
+        } else if (pins.digitalReadPin(DigitalPin.P1) == 1 && pins.digitalReadPin(DigitalPin.P8) == 0) {
+            Left()
+        } else if (pins.digitalReadPin(DigitalPin.P1) == 1 && pins.digitalReadPin(DigitalPin.P8) == 1) {
+            section += 1
+            basic.showNumber(section)
+            Foward()
+            basic.pause(250)
+            if (section == 2) {
+                section2()
+            }
+        }
     }
 })
