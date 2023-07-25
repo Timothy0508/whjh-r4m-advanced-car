8750a9f331144e035384b91debd11b9f9b5a7fb7
Timothy0508 <99249271+Timothy0508@users.noreply.github.com>
2023-07-25 13:04:06 +0800

    更新 pxt.json, main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -2,28 +2,52 @@ def Left():
     sensors.dd_mmotor(AnalogPin.P13, 1, AnalogPin.P14, 127)
     sensors.dd_mmotor(AnalogPin.P15, 1, AnalogPin.P16, 127)
 def Foward():
-    sensors.dd_mmotor(AnalogPin.P13, 0, AnalogPin.P14, 255)
-    sensors.dd_mmotor(AnalogPin.P15, 1, AnalogPin.P16, 255)
+    sensors.dd_mmotor(AnalogPin.P13, 0, AnalogPin.P14, 127)
+    sensors.dd_mmotor(AnalogPin.P15, 1, AnalogPin.P16, 127)
 def section2():
+    global start
     Foward()
     basic.pause(500)
-    Foward()
+    fowardLeft()
     basic.pause(600)
-
-def on_pin_pressed_p2():
-    global start
-    start = 1
-input.on_pin_pressed(TouchPin.P2, on_pin_pressed_p2)
-
-def section4():
-    Right()
-    basic.pause(1500)
+    Stop()
     Backward()
     basic.pause(500)
     Foward()
-    basic.pause(500)
-    Left()
+    basic.pause(2500)
+    Stop()
+    start = 0
+
+def on_button_pressed_a():
+    global step
+    step += 1
+input.on_button_pressed(Button.A, on_button_pressed_a)
+
+def on_pin_pressed_p2():
+    global section, start
+    section = 1
+    basic.show_number(section)
+    Foward()
+    basic.pause(2000)
+    section = 1
+    start = 1
+    
+input.on_pin_pressed(TouchPin.P2, on_pin_pressed_p2)
+
+def fowardLeft():
+    sensors.dd_mmotor(AnalogPin.P13, 1, AnalogPin.P14, 255)
+    sensors.dd_mmotor(AnalogPin.P15, 1, AnalogPin.P16, 127)
+def section4():
+    global start
+    Foward()
     basic.pause(1500)
+    fowardLeft()
+    basic.pause(600)
+    Stop()
+    Foward()
+    basic.pause(2500)
+    Stop()
+    start = 0
 def Right():
     sensors.dd_mmotor(AnalogPin.P13, 0, AnalogPin.P14, 127)
     sensors.dd_mmotor(AnalogPin.P15, 0, AnalogPin.P16, 127)
@@ -31,21 +55,38 @@ def Stop():
     sensors.dd_mmotor(AnalogPin.P13, 0, AnalogPin.P14, 0)
     sensors.dd_mmotor(AnalogPin.P15, 1, AnalogPin.P16, 0)
 def section5():
-    Right()
-    basic.pause(1500)
+    global start
     Foward()
-    basic.pause(200)
-    Backward()
-    basic.pause(800)
+    basic.pause(1000)
+    fowardLeft()
+    basic.pause(600)
     Stop()
-    basic.pause(99999999999999999999999)
+    Backward()
+    basic.pause(500)
+    Stop()
+    start = 0
 def Backward():
-    sensors.dd_mmotor(AnalogPin.P13, 1, AnalogPin.P14, 255)
-    sensors.dd_mmotor(AnalogPin.P15, 0, AnalogPin.P16, 255)
+    sensors.dd_mmotor(AnalogPin.P13, 1, AnalogPin.P14, 127)
+    sensors.dd_mmotor(AnalogPin.P15, 0, AnalogPin.P16, 127)
 section = 0
+step = 0
 start = 0
 start = 0
 pins.set_pull(DigitalPin.P2, PinPullMode.PULL_UP)
+
+def section6():
+    global start
+    Foward()
+    basic.pause(1000)
+    fowardLeft()
+    basic.pause(600)
+    Stop()
+    Backward()
+    basic.pause(500)
+    Foward()
+    basic.pause(2500)
+    Stop()
+    start = 0
 
 def on_forever():
     global section
@@ -58,11 +99,27 @@ def on_forever():
             Left()
         elif pins.digital_read_pin(DigitalPin.P1) == 1 and pins.digital_read_pin(DigitalPin.P8) == 1:
             section += 1
+            Foward()
+            basic.pause(1500)
             basic.show_number(section)
             if section == 2:
-                section2()
+                section = 2
+                if step == 1:
+                    basic.clear_screen()
+                    section2()
             elif section == 4:
-                section4()
+                section = 4
+                if step == 2:
+                    section4()
             elif section == 5:
-                section5()
+                section = 5
+                if step == 3:
+                    section5()
+            elif section == 6:
+                section = 6
+                if step = 4:
+                    pass
+    else:
+        basic.show_number(step)
+        Stop()
 basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -30,16 +30,22 @@ function section2 () {
     Foward()
     basic.pause(1000)
     fowardLeft()
-    basic.pause(10)
+    basic.pause(600)
+    Stop()
+    Backward()
+    basic.pause(500)
+    Foward()
+    basic.pause(2500)
+    Stop()
     start = 0
 }
 input.onButtonPressed(Button.A, function () {
     step += 1
 })
 input.onPinPressed(TouchPin.P2, function () {
-    section += 1
+    section += 0
     Foward()
-    basic.pause(2000)
+    basic.pause(500)
     start = 1
 })
 function fowardLeft () {
@@ -57,13 +63,15 @@ function fowardLeft () {
     )
 }
 function section4 () {
-    Right()
-    basic.pause(1500)
-    basic.pause(500)
     Foward()
-    basic.pause(500)
-    Left()
     basic.pause(1500)
+    fowardLeft()
+    basic.pause(600)
+    Stop()
+    Foward()
+    basic.pause(2500)
+    Stop()
+    start = 0
 }
 function Right () {
     sensors.DDMmotor(
@@ -94,13 +102,15 @@ function Stop () {
     )
 }
 function section5 () {
-    Right()
-    basic.pause(1200)
     Foward()
-    basic.pause(200)
-    basic.pause(800)
+    basic.pause(1000)
+    fowardLeft()
+    basic.pause(600)
     Stop()
-    basic.pause(99999999999999999999999)
+    Backward()
+    basic.pause(500)
+    Stop()
+    start = 0
 }
 function Backward () {
     sensors.DDMmotor(
@@ -131,19 +141,27 @@ basic.forever(function () {
             Left()
         } else if (pins.digitalReadPin(DigitalPin.P1) == 1 && pins.digitalReadPin(DigitalPin.P8) == 1) {
             section += 1
+            Foward()
+            basic.pause(1500)
+            basic.showNumber(section)
             if (section == 2) {
+                section = 2
                 if (step == 1) {
                     basic.clearScreen()
                     section2()
                 }
             } else if (section == 4) {
+                section = 4
                 if (step == 2) {
                     section4()
                 }
             } else if (section == 5) {
+                section = 5
                 if (step == 3) {
                     section5()
                 }
+            } else if (section == 6) {
+                section = 6
             }
         }
     } else {
